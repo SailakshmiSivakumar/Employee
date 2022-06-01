83faeaf739c21f1f23dd2ba692b17e23e24f8ff9
HISTOINDIA\Sailakshmis <SailakshmiSivakumar@histogenetics.com>
2022-06-01 11:25:17 +0530

    Added test

--- src/main/java/Com/EmployeeCrud/EmpController/EmployeeController.java
+++ src/main/java/Com/EmployeeCrud/EmpController/EmployeeController.java
@@ -1,9 +1,13 @@
 package Com.EmployeeCrud.EmpController;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController
 {
-
+	@GetMapping("/test")
+	public String test() {
+		return "Test App";
+	}
 }
