d876421efe0b0ee17acb0f4f43df069c91d15949
HISTOINDIA\Sailakshmis <SailakshmiSivakumar@histogenetics.com>
2022-06-01 13:05:53 +0530

    Save method added

--- src/main/java/Com/EmployeeCrud/EmpController/EmployeeController.java
+++ src/main/java/Com/EmployeeCrud/EmpController/EmployeeController.java
@@ -1,13 +1,31 @@
 package Com.EmployeeCrud.EmpController;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import Com.EmployeeCrud.EmployeeModel.Employee;
+import Com.EmployeeCrud.EmployeeService.EmployeeService;
 
 @RestController
 public class EmployeeController
 {
+	EmployeeService employeeservice;
 	@GetMapping("/test")
 	public String test() {
 		return "Test App";
 	}
+	@PostMapping("/add")
+	public int addemp(@RequestBody Employee employee) {
+	
+		return employeeservice.add(employee);
+	}
+
+	
 }
--- src/main/java/Com/EmployeeCrud/EmployeeService/EmployeeService.java
+++ src/main/java/Com/EmployeeCrud/EmployeeService/EmployeeService.java
@@ -1,7 +1,12 @@
 package Com.EmployeeCrud.EmployeeService;
 
+import java.util.List;
+
+import Com.Employeecrud.Model.Employee;
+
 public interface EmployeeService 
 {
-	
+
+public	int add(Employee employee);
 
 }
